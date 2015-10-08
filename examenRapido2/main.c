a76df5e12174c90e87f91acac2cd98cb8e75709b
rodericksolana <a01129839@itesm.mx>
2015-10-08 16:36:03 -0500

    actualizado Corriendo BIEN!

--- examenRapido2/main.c
+++ examenRapido2/main.c
@@ -6,6 +6,10 @@ int z=0;
 int i=10; 
 int s=0;
 int j=1;
+void valor(void)
+{
+s=1;
+}
 void gestor_ctrlc(int senial)
 {
    
@@ -33,31 +37,46 @@ int main(int argc, const char * argv[])
         printf("error crenado el manejador.\n");
         exit(-1); 
     }
+if ( signal (SIGINT, SIG_IGN) == SIG_ERR )//cambio el comportamiento para ignorar la señal
+    {
+        printf("error crenado el manejador.\n");
+        exit(-1); 
+    }
   
+  
+
     pid_t pid = fork();
 while(1)
 {
     if(!pid)
     {
-	for(;i>0;--i)
+	for(;i>=0;--i)
     {
+if(i==0)
+{
+printf("soy el hijo y Desperté!");
+    kill(getppid(), SIGUSR1); 
+}
         sleep(1);
         printf("%d segundo durmiendo Hijo\n",i);
     }
-printf("soy el hijo y Desperté!");
-s=1;
 
-
-    kill(getpid(), SIGKILL); 
-printf("soy el hijo y Morí!");
-     
-    
-        
     }//Cierre hijo
     else
     {
-        //padre
+if ( signal (SIGINT, gestor_ctrlc) == SIG_ERR )//cambio el comportamiento para ignorar la señal
+    {
+       printf("Error en el gestor de señales.\n");
+        exit(-1); 
+    }
 
+   if ( signal (SIGTSTP, gestor_ctrlz) == SIG_ERR )//cambio el comportamiento para ignorar la señal
+    {
+       printf("Error en el gestor de señales.\n");
+        exit(-1); 
+    }
+        //padre
+signal(SIGUSR1, valor);
   if ( s==1 )//cambio el comportamiento para ignorar la señal
         {
         printf("Se presionó Ctrl C  %d  veces. y Ctrl Z  %d  veces.\n", c , z); 
@@ -71,17 +90,7 @@ for(;j<=x;++j)
     }
 printf("Aparezco  %d  segundos \n", x);
 j=1;
-   if ( signal (SIGINT, gestor_ctrlc) == SIG_ERR )//cambio el comportamiento para ignorar la señal
-    {
-       printf("Error en el gestor de señales.\n");
-        exit(-1); 
-    }
-
-   if ( signal (SIGTSTP, gestor_ctrlz) == SIG_ERR )//cambio el comportamiento para ignorar la señal
-    {
-       printf("Error en el gestor de señales.\n");
-        exit(-1); 
-    }
+   
        
        
         
